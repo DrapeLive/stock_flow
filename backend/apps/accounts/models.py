e1ee218ba068ce18ad05983a7f2225968c2ae7c9
Muflih-uk <muhammedmuflih875@gmail.com>
2025-12-23 12:49:49 +0530

    add: Add the Email Validation

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -9,7 +9,13 @@ class User(AbstractUser):
         ('AGENT', 'Agent')
     )
 
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = 'ADMIN'
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.username} ({self.role})"
