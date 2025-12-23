2c832b969977f9f75197cd7f65e944580ce886ba
Muflih-uk <muhammedmuflih875@gmail.com>
2025-12-23 10:13:15 +0530

    feat: items app added in global urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.accounts.urls')),
     path('api/agents/',include('apps.agents.urls')),
-    path('api/customers/', include('apps.customers.urls'))
+    path('api/customers/', include('apps.customers.urls')),
+    path('api/items/', include('apps.items.urls'))
 ]
