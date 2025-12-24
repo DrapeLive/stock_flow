18c5320b94322d416ec32b93f3b5c43c0a2abe92
Muflih-uk <muhammedmuflih875@gmail.com>
2025-12-24 19:06:51 +0530

    fix: fix the error unknown class in urls

--- backend/apps/orders/urls.py
+++ backend/apps/orders/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, AddOrderItemView, ChangeOrderStatusView
+from .views import OrderViewSet, AddOrderItemView
 
 router = DefaultRouter()
 router.register('', OrderViewSet, basename='orders')
 
 urlpatterns = router.urls + [
     path('<int:order_id>/add-item/', AddOrderItemView.as_view()),
-    path('<int:order_id>/status/', ChangeOrderStatusView.as_view()),
 ]
