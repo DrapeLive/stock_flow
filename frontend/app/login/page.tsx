da9b0b4f78deb1a0e3d34c68cadb7ffc0deb4701
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2026-01-13 20:32:49 +0530

    feat: Add login route

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,31 @@
+import StockFlowButton from "@/components/ui/custom/stockFlowButton";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { LogIn, X } from "lucide-react";
+
+export default function Login() {
+  return (
+    <div className="flex min-h-screen min-w-full py-6 px-6 justify-center items-center">
+      <div className="flex flex-col w-full gap-16">
+        <h2 className="flex text-4xl w-full justify-center text-(--color-primary)">
+          Login
+        </h2>
+        <div className="flex flex-col gap-6">
+          <Field>
+            <FieldLabel>Email</FieldLabel>
+            <Input type="text" placeholder="example@gmail.com"></Input>
+          </Field>
+          <Field>
+            <FieldLabel>Password</FieldLabel>
+            <Input type="password" placeholder="*****"></Input>
+          </Field>
+
+          <div className="flex justify-center gap-2.5">
+            <StockFlowButton variant="outline" text="Back" icon={<X />} />
+            <StockFlowButton variant="filled" text="Login" icon={<LogIn />} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
