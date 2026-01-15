8bf82c9847ac76614b0d90c97ebb5dcfda3df80e
Muflih-uk <muhammedmuflih875@gmail.com>
2026-01-15 14:16:53 +0530

    feat(auth): create Reset Password Page

---
+++ frontend/app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Stock Flow",
+  description: "Manage all your orders in one place",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased  py-4 px-5 `}>
+        {children}
+      </body>
+    </html>
+  );
+}
