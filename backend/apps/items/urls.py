3805db4e8426fc128fbb25d32ac812b79b81ed6e
Muflih-uk <muhammedmuflih875@gmail.com>
2026-01-15 11:18:31 +0530

    refactor(items): add the variants in items and change the models

--- backend/apps/items/urls.py
+++ backend/apps/items/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+from .views import ItemViewSet, ItemVariantViewSet
 
 router = DefaultRouter()
-router.register('', ItemViewSet)
+router.register(r'', ItemViewSet)
+router.register(r'variants', ItemVariantViewSet)
 
 urlpatterns = router.urls
