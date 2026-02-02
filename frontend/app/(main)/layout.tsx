d2a2fbe5a871e4356fb09d1201299f04f6a4269f
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-02 23:37:18 +0530

    refactor: modify the each layouts

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -1,30 +1,14 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "../globals.css";
 import NavBar from "../../components/ui/NavBar";
 
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-});
-
-export const metadata: Metadata = {
-  title: "Stock Flow",
-  description: "Manage all your orders in one place",
-};
-
-export default function RootLayout({
+export default function MainLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${poppins.variable} antialiased bg-linear-to-t from-[#EEEEEE] to-white`}>
-        {children}
-        <NavBar />
-      </body>
-    </html>
+    <div className="main-layout">
+      {children}
+      <NavBar />
+    </div>
   );
 }
--- frontend/app/(order)/layout.tsx
+++ frontend/app/(order)/layout.tsx
@@ -1,35 +1,30 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "../globals.css";
 import Link from "next/link";
 import { X } from "lucide-react";
 
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-});
-export const metadata: Metadata = {
-  title: "Stock Flow",
-  description: "Manage all your orders in one place",
-};
-
-export default function RootLayout({
+export default function OrderLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${poppins.variable} antialiased  py-4 px-5 `}>
-        <h1 className="text-(--color-primary) flex justify-center">Create new Order</h1>
-        {children}
-        <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full">
-            <div className="w-full flex justify-center pb-5">
-                <Link  href="/" className="flex gap-1 text-(--color-primary) border border-(--color-primary) p-1.5 rounded-md">Cancel <span><X/></span></Link>
-            </div>
+    <div className="admin-order-layout">
+      <h1 className="text-(--color-primary) flex justify-center">
+        Create new Order
+      </h1>
+      {children}
+      <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full">
+        <div className="w-full flex justify-center pb-5">
+          <Link
+            href="/"
+            className="flex gap-1 text-(--color-primary) border border-(--color-primary) p-1.5 rounded-md"
+          >
+            Cancel{" "}
+            <span>
+              <X />
+            </span>
+          </Link>
         </div>
-      </body>
-    </html>
+      </div>
+    </div>
   );
 }
