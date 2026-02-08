e31d7d86c59544ab68492a7b52f613bf9ce554fa
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-08 21:27:08 +0530

    feat: fetch the orders

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Filter, Search } from "lucide-react";
+import { Filter, Info, Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react";
 import { orderApi } from "@/lib/api/order";
@@ -8,11 +8,12 @@ import { OrderAllResponse } from "@/types/order";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import groupOrders from "@/util/groupOrders";
 
 export default function Home() {
   const { isAuthenticated } = useAuth();
 
-  const [data, setData] = useState<OrderAllResponse>();
+  const [data, setData] = useState<OrderAllResponse>([]);
   const [loading, setLoading] = useState(true);
 
   const router = useRouter();
@@ -40,6 +41,10 @@ export default function Home() {
     fetchData();
   }, [isAuthenticated, router]);
 
+  const { pendingPacked } = groupOrders(data ?? []);
+
+  const order_len = pendingPacked.length;
+
   if (loading) return <p>Loading</p>;
 
   return (
@@ -58,26 +63,22 @@ export default function Home() {
         <div className="flex gap-1 items-center">
           <p>Remaining Order</p>
           <div className="bg-(--color-border) rounded-full py-0.5 px-2">
-            <p className="font-bold">40</p>
+            <p className="font-bold">{order_len}</p>
           </div>
         </div>
         <div className="p-0.5 rounded-[3px] border border-(--color-border)">
           <Filter className="text-(--color-border) w-2.5 h-2.5" />
         </div>
       </div>
-      <div className="space-y-3 pt-3">
-        {data?.map((order) => {
+      <div className="space-y-2 pt-2">
+        {pendingPacked?.map((order) => {
           const previewImages = order.items.slice(0, 3);
 
           return (
-            <div
-              key={order.id}
-              className="flex items-center justify-between p-3 border rounded-xl shadow-sm"
-            >
-              {/* LEFT — stacked images */}
-              <div className="relative w-20 h-16">
+            <div key={order.id} className="flex p-1 border-b">
+              <div className="relative w-16 h-16">
                 {previewImages[0] && (
-                  <div className="absolute left-0 z-30 rotate-[-12deg]">
+                  <div className="absolute left-0 z-30 rotate-0">
                     <Image
                       src={previewImages[0].variant.image}
                       alt={previewImages[0].item.name}
@@ -89,7 +90,7 @@ export default function Home() {
                 )}
 
                 {previewImages[1] && (
-                  <div className="absolute left-3 z-20 rotate-[6deg]">
+                  <div className="absolute left-0 z-20 rotate-10">
                     <Image
                       src={previewImages[1].variant.image}
                       alt={previewImages[1].item.name}
@@ -101,7 +102,7 @@ export default function Home() {
                 )}
 
                 {previewImages[2] && (
-                  <div className="absolute left-6 z-10 rotate-0">
+                  <div className="absolute left-0 z-10 rotate-20">
                     <Image
                       src={previewImages[2].variant.image}
                       alt={previewImages[2].item.name}
@@ -113,16 +114,36 @@ export default function Home() {
                 )}
               </div>
 
-              {/* CENTER */}
-              <div className="flex-1 px-3">
-                <p className="font-semibold">{order.customer_details.name}</p>
-                <p className="text-sm text-muted-foreground">
-                  {order.items.length} items
+              <div className="flex flex-col px-2 justify-center gap-2 max-h-full">
+                <h6 className="font-medium">{order.customer_details.name}</h6>
+                <p className="max-w-30 truncate whitespace-nowrap">
+                  {order.items.map((item) => (
+                    <span className="text-(--color-text) mr-1" key={item.id}>
+                      {item.item.name}
+                    </span>
+                  ))}
                 </p>
               </div>
 
-              {/* RIGHT */}
-              <div className="text-lg font-bold">{order.total_quantity}</div>
+              <div className="flex items-center pr-2">
+                <div
+                  className={
+                    order.status == "PENDING"
+                      ? "bg-(--color-pending)/20 border border-(--color-pending) text-(--color-pending) rounded-full px-1 py-0.5"
+                      : "bg-(--color-packed)/20 border border-(--color-packed) text-(--color-packed) rounded-full px-1 py-0.5"
+                  }
+                >
+                  <p>{order.status}</p>
+                </div>
+              </div>
+
+              <div className="px-4 flex text-center justify-center flex-col">
+                <h3>{order.total_quantity}</h3>
+                <p className="text-(--color-text)">Pieces</p>
+              </div>
+              <div className="flex justify-center items-center">
+                <Info />
+              </div>
             </div>
           );
         })}
