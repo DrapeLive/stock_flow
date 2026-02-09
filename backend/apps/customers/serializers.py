610f09a20dc263ed3807d61192a4b566aa2c382c
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-09 21:42:58 +0530

    refactor: change the response of customer

--- backend/apps/customers/serializers.py
+++ backend/apps/customers/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Customer
+from apps.orders.models import Order
 
 class CustomerSerializer(serializers.ModelSerializer):
+    total_orders = serializers.SerializerMethodField()
+    agent_name = serializers.CharField(source="agent.user.get_name", read_only=True)
+
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def get_total_orders(self, obj):
+        return Order.objects.filter(customer=obj).count()
