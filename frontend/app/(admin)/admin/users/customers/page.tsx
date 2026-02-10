5cad36ae4a1c2f49e5d4f2518d31a953e2c55b72
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-10 19:02:09 +0530

    feat: admin customer and agent api connected

--- frontend/app/(admin)/admin/users/customers/page.tsx
+++ frontend/app/(admin)/admin/users/customers/page.tsx
@@ -1,30 +1,37 @@
-import { Plus } from "lucide-react";
+"use client";
+import AgentsList from "@/components/pages/admin/users/AgentsList";
+import CustomerList from "@/components/pages/admin/users/CustomersList";
+import { useState } from "react";
+
+type Tab = "Customers" | "Agents";
 
 export default function CustomersPage() {
+  const [activeTab, setActiveTab] = useState<Tab>("Customers");
+
+  const renderContent = (): React.ReactNode => {
+    if (activeTab == "Agents") {
+      return <AgentsList />;
+    } else if (activeTab == "Customers") {
+      return <CustomerList />;
+    }
+  };
   return (
     <div className="min-h-screen min-w-full">
       <div className="border p-1 mt-2 flex items-center justify-center space-x-1 border-(--color-border) rounded-full">
-        <button className="bg-(--color-primary) rounded-full px-13.5 py-1 text-white font-semibold text-xs">
+        <button
+          onClick={() => setActiveTab("Customers")}
+          className={` rounded-full px-13.5 py-1 font-semibold text-xs ${activeTab == "Customers" ? "bg-(--color-primary) text-white" : "bg-white text-black"} `}
+        >
           Customers
         </button>
-        <button className="bg-(--color-primary) rounded-full px-13.5 py-1 text-white font-semibold text-xs">
+        <button
+          onClick={() => setActiveTab("Agents")}
+          className={`bg-(--color-primary) rounded-full px-13.5 py-1 font-semibold text-xs ${activeTab == "Agents" ? "bg-(--color-primary) text-white" : "bg-white text-black"}`}
+        >
           Agents
         </button>
       </div>
-      <div className="pt-2 flex justify-between">
-        <div className="flex gap-1 items-center">
-          <p>Remaining Order</p>
-          <div className="bg-(--color-border) rounded-full py-0.5 px-2">
-            <p className="font-bold">40</p>
-          </div>
-        </div>
-        <button className="p-1 rounded-md bg-(--color-primary) text-white border border-(--color-border)">
-          <div className="flex items-center gap-1">
-            <p>Add new Customers</p>
-            <Plus size={16} />
-          </div>
-        </button>
-      </div>
+      <div>{renderContent()}</div>
     </div>
   );
 }
