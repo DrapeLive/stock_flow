ea81a4537a42003c43d034adedd611a8a570111d
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-10 19:04:20 +0530

    feat: create component List Agent and Customer

---
+++ frontend/components/pages/admin/users/CustomersList.tsx
@@ -0,0 +1,24 @@
+import { Plus } from "lucide-react";
+
+const CustomerList: React.FC = () => {
+  return (
+    <>
+      <div className="pt-2 flex justify-between">
+        <div className="flex gap-1 items-center">
+          <p>Remaining Order</p>
+          <div className="bg-(--color-border) rounded-full py-0.5 px-2">
+            <p className="font-bold">40</p>
+          </div>
+        </div>
+        <button className="p-1 rounded-md bg-(--color-primary) text-white border border-(--color-border)">
+          <div className="flex items-center gap-1">
+            <p>Add new Customers</p>
+            <Plus size={16} />
+          </div>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CustomerList;
