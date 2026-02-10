540697b68f347bf35082fbf6b6b0e0fda1ae0cc4
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-10 19:01:46 +0530

    feat: admin homepage api connected

--- frontend/app/(admin)/admin/page.tsx
+++ frontend/app/(admin)/admin/page.tsx
@@ -1,33 +1,58 @@
-import { Filter } from "lucide-react";
+"use client";
+
+import { useState } from "react";
+import Packed from "@/components/pages/admin/order_components/Packed";
+import Pending from "@/components/pages/admin/order_components/Pending";
+import Dispatched from "@/components/pages/admin/order_components/Dispatched";
+import All from "@/components/pages/admin/order_components/All";
+
+type Tab = "All" | "Packed" | "Pending" | "Dispatched";
 
 export default function AdminHomePage() {
+  const [activeTab, setActiveTab] = useState<Tab>("All");
+
+  const renderContent = (): React.ReactNode => {
+    switch (activeTab) {
+      case "Packed":
+        return <Packed />;
+      case "Pending":
+        return <Pending />;
+      case "Dispatched":
+        return <Dispatched />;
+      default:
+        return <All />;
+    }
+  };
+
   return (
     <div className="min-h-screen min-w-full">
       <div className="border p-1 mt-2 flex items-center justify-between space-x-1 border-(--color-border) rounded-full">
-        <button className="bg-(--color-primary) rounded-full px-4 py-0.5 text-white font-semibold text-xs">
+        <button
+          onClick={() => setActiveTab("All")}
+          className={` rounded-full px-4 py-0.5 font-semibold text-xs ${activeTab == "All" ? "bg-(--color-primary) text-white" : "text-black bg-white"}`}
+        >
           All
         </button>
-        <button className="bg-(--color-primary) rounded-full px-4 py-0.5 text-white font-semibold text-xs">
+        <button
+          onClick={() => setActiveTab("Pending")}
+          className={` rounded-full px-4 py-0.5  font-semibold text-xs ${activeTab == "Pending" ? "bg-(--color-primary) text-white" : "text-black bg-white"}`}
+        >
           Pending
         </button>
-        <button className="bg-(--color-primary) rounded-full px-4 py-0.5 text-white font-semibold text-xs">
+        <button
+          onClick={() => setActiveTab("Packed")}
+          className={` rounded-full px-4 py-0.5 font-semibold text-xs ${activeTab == "Packed" ? "bg-(--color-primary) text-white" : "text-black bg-white"}`}
+        >
           Packed
         </button>
-        <button className="bg-(--color-primary) rounded-full px-4 py-0.5 text-white font-semibold text-xs">
+        <button
+          onClick={() => setActiveTab("Dispatched")}
+          className={` rounded-full px-4 py-0.5 font-semibold text-xs ${activeTab == "Dispatched" ? "bg-(--color-primary) text-white" : "text-black bg-white"}`}
+        >
           Dispatched
         </button>
       </div>
-      <div className="pt-2 flex justify-between">
-        <div className="flex gap-1 items-center">
-          <p>Remaining Order</p>
-          <div className="bg-(--color-border) rounded-full py-0.5 px-2">
-            <p className="font-bold">40</p>
-          </div>
-        </div>
-        <div className="p-0.5 rounded-[3px] border border-(--color-border)">
-          <Filter className="text-(--color-border) w-2.5 h-2.5" />
-        </div>
-      </div>
+      <div>{renderContent()}</div>
     </div>
   );
 }
