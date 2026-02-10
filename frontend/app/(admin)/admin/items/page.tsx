b9e34aaeca1068064c396530f8af7da80947fa72
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-10 21:12:57 +0530

    feat: create the api for list items

--- frontend/app/(admin)/admin/items/page.tsx
+++ frontend/app/(admin)/admin/items/page.tsx
@@ -1,22 +1,9 @@
-import { Plus } from "lucide-react";
+import ListItems from "@/components/pages/admin/items/ListItems";
 
 export default function ItemsPage() {
   return (
     <div className="min-h-screen min-w-full">
-      <div className="pt-2 flex justify-between">
-        <div className="flex gap-1 items-center">
-          <p>Total Item</p>
-          <div className="bg-(--color-border) rounded-full py-0.5 px-2">
-            <p className="font-bold">40</p>
-          </div>
-        </div>
-        <button className="p-1 rounded-md bg-(--color-primary) text-white border border-(--color-border)">
-          <div className="flex items-center gap-1">
-            <p>Add new Item</p>
-            <Plus size={16} />
-          </div>
-        </button>
-      </div>
+      <ListItems />
     </div>
   );
 }
