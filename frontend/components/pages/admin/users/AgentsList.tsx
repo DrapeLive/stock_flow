2fef6ec6d9f3d0ad3661ed6f93bf6d48f2e894b6
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-10 21:16:03 +0530

    feat: AgentItem Component Created

--- frontend/components/pages/admin/users/AgentsList.tsx
+++ frontend/components/pages/admin/users/AgentsList.tsx
@@ -1,13 +1,54 @@
-import { Plus } from "lucide-react";
+"use client";
+import { useAuth } from "@/context/AuthContext";
+import { agentApi } from "@/lib/api/agents";
+import { AgentAllResponse } from "@/types/agent";
+import { Info, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const AgentsList: React.FC = () => {
+  const { isAuthenticated } = useAuth();
+
+  const [data, setData] = useState<AgentAllResponse>([]);
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (!isAuthenticated) {
+      router.push("/login");
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await agentApi.getAll();
+        setData(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [isAuthenticated, router]);
+
+  if (loading) {
+    return <h2 className="flex justify-center">Loading</h2>;
+  }
+  if (data.length == 0)
+    return <h2 className="flex justify-center">No Agents</h2>;
+
   return (
     <>
-      <div className="pt-2 flex justify-between">
+      <div className="py-2 flex justify-between">
         <div className="flex gap-1 items-center">
-          <p>Remaining Order</p>
+          <p>Remaining Agents</p>
           <div className="bg-(--color-border) rounded-full py-0.5 px-2">
-            <p className="font-bold">40</p>
+            <p className="font-bold">{data.length}</p>
           </div>
         </div>
         <button className="p-1 rounded-md bg-(--color-primary) text-white border border-(--color-border)">
@@ -16,6 +57,27 @@ const AgentsList: React.FC = () => {
             <Plus size={16} />
           </div>
         </button>
+      </div>
+      <div>
+        {data?.map((item, index) => (
+          <div
+            className="flex border-b border-(--color-border) py-2"
+            key={index}
+          >
+            <div className="min-w-38">
+              <h6>{item.user.username}</h6>
+              <p className="text-(--color-text)">{item.user.email}</p>
+            </div>
+            <div className="flex w-full justify-between">
+              <h6 className="flex items-center">+91 {item.contact}</h6>
+              <div className="flex flex-col items-center">
+                <h3>{item.total_customers}</h3>
+                <p className="text-(--color-text)">Customers</p>
+              </div>
+              <Info />
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
