1693075e141245d7802f04d4b46d0eec0f081b6d
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-11 17:01:39 +0530

    feat: create a permission

--- backend/apps/accounts/permissions.py
+++ backend/apps/accounts/permissions.py
@@ -7,3 +7,13 @@ class IsAgent(BasePermission):
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'ADMIN'
+class IsAdminOrSelfAgent(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.role == "ADMIN":
+            return True
+
+        return obj.user == request.user
