444709eac4634239f596b0b02a2ef462d26ededb
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-11 16:48:41 +0530

    feat: add api in agent profile

--- frontend/app/(main)/profile/page.tsx
+++ frontend/app/(main)/profile/page.tsx
@@ -1,36 +1,73 @@
-import { ChevronRight, LogOut, User } from "lucide-react";
+"use client";
+import { useAuth } from "@/context/AuthContext";
+import { agentApi } from "@/lib/api/agents";
+import { AgentResponse } from "@/types/agent";
+import { ChevronRight, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function Profile(){
-    return(
-        <div className="min-h-screen min-w-full py-3 px-3">
-            <div className="text-center">
-                <h1>Profile</h1>
-            </div>
-            <div className="flex justify-center items-center pb-6">
+export default function Profile() {
+  const { isAuthenticated, user } = useAuth();
+
+  const [data, setData] = useState<AgentResponse>();
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (!isAuthenticated) {
+      router.push("/login");
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await agentApi.getOne(user?.id);
+        setData(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [isAuthenticated, router, user]);
+
+  if (loading) <h2 className="flex justify-center">Not Found</h2>;
+
+  return (
+    <div className="min-h-screen min-w-full py-3 px-3">
+      <div className="text-center">
+        <h1>Profile</h1>
+      </div>
+      {/*<div className="flex justify-center items-center pb-6">
                 <User className="w-45 h-45 bg-(--color-border) rounded-full"/>
-            </div>
-            <div className="flex flex-col items-center gap-1.5 pb-10">
-                <h3>John Doe</h3>
-                <h3 className="text-(--color-text)">something@gmail.com</h3>
-                <h3 className="text-(--color-text)">+91 9733022122</h3>
-            </div>
-            <div className="px-2">
-                <button className="flex justify-between items-center pb-2 w-full">
-                    <h3>Customers</h3>
-                    <div className="flex items-center justify-center gap-1">
-                        <h3 className="leading-none m-0">8</h3>
-                        <ChevronRight className="w-5 h-5" />
-                    </div>
-                </button>
-                <button className="flex justify-between items-center pb-5 w-full">
-                    <h3>Change Password</h3>
-                    <ChevronRight className="w-5 h-5" />
-                </button>
-                <button className="flex text-(--color-pending) justify-between items-center w-full">
-                    <h3 className="text-(--color-pending)">Logout</h3>
-                    <LogOut className="w-5 h-5" />
-                </button>
-            </div>
-        </div>
-    );
+            </div>*/}
+      <div className="flex flex-col items-center gap-1.5 pb-10">
+        <h3>{data?.user.username}</h3>
+        <h3 className="text-(--color-text)">{data?.user.email}</h3>
+        <h3 className="text-(--color-text)">+91 {data?.contact}</h3>
+      </div>
+      <div className="px-2">
+        <button className="flex justify-between items-center pb-2 w-full">
+          <h3>Customers</h3>
+          <div className="flex items-center justify-center gap-1">
+            <h3 className="leading-none m-0">{data?.total_customers}</h3>
+            <ChevronRight className="w-5 h-5" />
+          </div>
+        </button>
+        <button className="flex justify-between items-center pb-5 w-full">
+          <h3>Change Password</h3>
+          <ChevronRight className="w-5 h-5" />
+        </button>
+        <button className="flex text-(--color-pending) justify-between items-center w-full">
+          <h3 className="text-(--color-pending)">Logout</h3>
+          <LogOut className="w-5 h-5" />
+        </button>
+      </div>
+    </div>
+  );
 }
