49096b5ce3e41a59264ad17eff506fa2f0e27584
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-11 16:48:19 +0530

    create: create type for agent

--- frontend/types/agent.ts
+++ frontend/types/agent.ts
@@ -2,3 +2,6 @@ import type { operations } from "@/types/api";
 
 export type AgentAllResponse =
   operations["agents_list"]["responses"][200]["content"]["application/json"];
+
+export type AgentResponse =
+  operations["agents_retrieve"]["responses"][200]["content"]["application/json"];
