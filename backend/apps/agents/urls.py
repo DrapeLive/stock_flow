55dae7e1fa4c39f04f2bb48464beea413bc31fff
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-11 20:18:19 +0530

    refactor: update agent profile

--- backend/apps/agents/urls.py
+++ backend/apps/agents/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AgentViewSet
+from .views import AgentViewSet, AgentDetail
 
 router = DefaultRouter()
-router.register('', AgentViewSet, basename='agent')
+router.register('agents', AgentViewSet, basename='agent')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('profile/<int:user_id>/', AgentDetail.as_view(), name='agent-by-user'),
+]
+
--- backend/apps/agents/views.py
+++ backend/apps/agents/views.py
@@ -1,7 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Agent
 from .serializers import AgentSerializer
 from apps.accounts.permissions import IsAdminOrSelfAgent
+from django.shortcuts import get_object_or_404
 
 
 class AgentViewSet(ModelViewSet):
@@ -15,3 +18,9 @@ class AgentViewSet(ModelViewSet):
             return Agent.objects.all()
 
         return Agent.objects.filter(user=user)
+
+class AgentDetail(APIView):
+    def get(self, request, user_id):
+        agent = get_object_or_404(Agent, user_id=user_id)
+        serializer = AgentSerializer(agent)
+        return Response(serializer.data)
