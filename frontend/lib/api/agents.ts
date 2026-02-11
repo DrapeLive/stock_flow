fa6fbc44e8c87e2cf096e58116182b21fffed9b2
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-11 16:48:57 +0530

    feat: create api for agent profile

--- frontend/lib/api/agents.ts
+++ frontend/lib/api/agents.ts
@@ -1,9 +1,14 @@
-import { AgentAllResponse } from "@/types/agent";
+import { AgentAllResponse, AgentResponse } from "@/types/agent";
 import { api } from "./axios";
 
 export const agentApi = {
   async getAll(): Promise<AgentAllResponse> {
     const res = await api.get<AgentAllResponse>("/api/agents/");
+    return res.data;
+  },
+
+  async getOne(id: number | undefined): Promise<AgentResponse> {
+    const res = await api.get<AgentResponse>(`/api/agents/${id}`);
     return res.data;
   },
 };
