669eadf608ab140f442ce6691348d14492b6b1cc
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-12 13:39:09 +0530

    refactor: order response body updated

--- frontend/types/order.ts
+++ frontend/types/order.ts
@@ -2,3 +2,9 @@ import type { operations } from "@/types/api";
 
 export type OrderAllResponse =
   operations["orders_list"]["responses"][200]["content"]["application/json"];
+
+export type OrderResponse =
+  operations["orders_retrieve"]["responses"][200]["content"]["application/json"];
+
+export type OrderItems =
+  operations["orders_retrieve"]["responses"][200]["content"]["application/json"]["items"];
