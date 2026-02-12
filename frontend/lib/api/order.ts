674e2d656d751ec5952d2bbd9772f12e6b170326
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-12 13:40:42 +0530

    create: created api for order

--- frontend/lib/api/order.ts
+++ frontend/lib/api/order.ts
@@ -1,9 +1,14 @@
-import { OrderAllResponse } from "@/types/order";
+import { OrderAllResponse, OrderResponse } from "@/types/order";
 import { api } from "./axios";
 
 export const orderApi = {
   async getAll(): Promise<OrderAllResponse> {
     const res = await api.get<OrderAllResponse>("/api/orders/");
+    return res.data;
+  },
+
+  async getOne(id: number): Promise<OrderResponse> {
+    const res = await api.get<OrderResponse>(`/api/orders/${id}`);
     return res.data;
   },
 };
