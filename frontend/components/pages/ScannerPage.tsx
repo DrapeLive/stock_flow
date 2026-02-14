c6c05360bbddac89da319a63ed00b3926e77a701
Muflih-uk <muhammedmuflih875@gmail.com>
2026-02-14 19:45:53 +0530

    feat: scanner api connected

--- frontend/components/pages/ScannerPage.tsx
+++ frontend/components/pages/ScannerPage.tsx
@@ -2,46 +2,55 @@
 
 import { Scanner } from "@yudiel/react-qr-scanner";
 import { useState } from "react";
-import { Image, X } from "lucide-react";
+import { Image } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-
-const ScannerPage: React.FC = () => {
-    
-    const [result, setResult] = useState<string | null>(null);
-
-    return(
-        <div className="h-full">
-
-            <div className="relative mx-auto w-58 h-58 rounded-3xl overflow-hidden bg-black">
-                <Scanner
-                    onScan={(data) => {
-                        if (data?.[0]?.rawValue) {
-                            setResult(data[0].rawValue);
-                        }
-                    }}
-                    onError={(err) => console.error(err)}
-                    classNames={{
-                        container: "w-full h-full",
-                        video: "w-full h-full object-cover",
-                    }}
-                />
-            </div>
-
-            <div className="pt-3 w-full flex justify-center">
-                <h1 className="font-medium text-(--color-primary)">Scan Item QR</h1>
-            </div>
-            <div className="flex flex-col text-(--color-primary) pb-3 items-center justify-center w-full">
-                <h4>Not Working?</h4>
-                <h4 className="font-medium">Enter Code Manually</h4>
-            </div>
-            <div className="flex w-full justify-center">
-                <button className="flex gap-1 text-(--color-primary) border border-(--color-primary) p-2 rounded-md">
-                    <h4 className="font-light">Scan from Image</h4>
-                    <Image/>
-                </button>
-            </div>
-        </div>
-    );
+interface ScannerPageProps {
+  id: string;
 }
+
+const ScannerPage: React.FC<ScannerPageProps> = ({ id }) => {
+  const [result, setResult] = useState<string | null>(null);
+
+  const router = useRouter();
+  return (
+    <div className="h-full">
+      <div className="relative mx-auto w-58 h-58 rounded-3xl overflow-hidden bg-black">
+        <Scanner
+          constraints={{
+            facingMode: "environment",
+            width: { ideal: 1280 },
+            height: { ideal: 720 }, // ← not exact, just preferred
+          }}
+          onScan={(data) => {
+            if (data?.[0]?.rawValue) {
+              setResult(data[0].rawValue);
+              router.push(`/order/new/${id}/${data}`);
+            }
+          }}
+          onError={(err) => console.error(err)}
+          classNames={{
+            container: "w-full h-full",
+            video: "w-full h-full object-cover",
+          }}
+        />
+      </div>
+
+      <div className="pt-3 w-full flex justify-center">
+        <h1 className="font-medium text-(--color-primary)">Scan Item QR</h1>
+      </div>
+      <div className="flex flex-col text-(--color-primary) pb-3 items-center justify-center w-full">
+        <h4>Not Working?</h4>
+        <h4 className="font-medium">Enter Code Manually</h4>
+      </div>
+      <div className="flex w-full justify-center">
+        <button className="flex gap-1 text-(--color-primary) border border-(--color-primary) p-2 rounded-md">
+          <h4 className="font-light">Scan from Image</h4>
+          <Image />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ScannerPage;
