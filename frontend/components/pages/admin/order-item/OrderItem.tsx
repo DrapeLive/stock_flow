a2bf918c89b60d7ba5e206125195631d94a5a053
Muflih-uk <muhammedmuflih875@gmail.com>
2026-03-01 12:42:21 +0530

    refactor: update Order Item Component

--- frontend/components/pages/admin/order-item/OrderItem.tsx
+++ frontend/components/pages/admin/order-item/OrderItem.tsx
@@ -1,20 +1,63 @@
+"use client";
+import { AlertDestructive } from "@/components/ui/AlertDestructive";
+import { PageLoading } from "@/components/ui/Loading";
+import { orderApi } from "@/lib/api/order";
 import { OrderItems } from "@/types/order";
+import { Trash } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 type Props = {
   items: OrderItems | undefined;
+  isDelete?: boolean;
+  orderId?: number;
 };
 
-const OrderItem: React.FC<Props> = ({ items }) => {
+const OrderItem: React.FC<Props> = ({ items, isDelete, orderId }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [orderItems, setOrderItems] = useState(items);
+
+  const onDelete = async (itemId: number, orderId?: number) => {
+    try {
+      setLoading(true);
+      await orderApi.deleteItem(String(orderId), String(itemId));
+
+      setOrderItems((prev) => prev?.filter((item) => item.id !== itemId));
+    } catch {
+      setError("Server Not Found");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <PageLoading />;
+
   return (
     <div className="pt-3 space-y-3">
-      {items?.map((item, index) => (
+      {error && <AlertDestructive heading="Error" description={error} />}
+      {orderItems?.map((item, index) => (
         <div
           className="flex w-full border-b border-(--color-border) py-2"
           key={index}
         >
+          {isDelete && (
+            <button
+              type="button"
+              onClick={() => onDelete(item.id, orderId)}
+              className="flex justify-center items-center p-2"
+            >
+              <Trash className="text-red-600 w-4 h-4" />
+            </button>
+          )}
           <div className="flex justify-between">
-            <Image src={item.variant.image} width={35} height={35} alt="" />
+            <Image
+              src={item.variant.image!}
+              width={45}
+              height={45}
+              alt=""
+              unoptimized
+            />
             <div className="pl-4 min-w-40">
               <h6>{item.item.name}</h6>
               <p>{item.variant.color}</p>
