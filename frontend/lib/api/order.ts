4cd50b2b0baac9e36a3f34cd5895eb31a0a8296f
Muflih-uk <muhammedmuflih875@gmail.com>
2026-03-01 12:41:07 +0530

    feat: create a api for delete Item

--- frontend/lib/api/order.ts
+++ frontend/lib/api/order.ts
@@ -1,6 +1,8 @@
 import {
   OrderAddItemResponse,
   OrderAllResponse,
+  OrderDeleteResponse,
+  OrderItemDeleteResponse,
   OrderItems,
   OrderRegisterRequest,
   OrderRegisterResponse,
@@ -27,6 +29,21 @@ export const orderApi = {
 
   async create(data: OrderRegisterRequest): Promise<OrderRegisterResponse> {
     const res = await api.post<OrderRegisterResponse>(`/api/orders/`, data);
+    return res.data;
+  },
+
+  async delete(id: string): Promise<OrderDeleteResponse> {
+    const res = await api.delete<OrderDeleteResponse>(`/api/orders/${id}/`);
+    return res.data;
+  },
+
+  async deleteItem(
+    orderId: string,
+    itemId: string,
+  ): Promise<OrderItemDeleteResponse> {
+    const res = await api.delete<OrderItemDeleteResponse>(
+      `/api/orders/${orderId}/delete-item/${itemId}/`,
+    );
     return res.data;
   },
 };
