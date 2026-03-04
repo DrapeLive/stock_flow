3bbb04f369741317531e33068bc322d604ea29cd
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2026-03-04 22:50:10 +0530

    refactor: Remove navbar when updating order status

--- frontend/app/(admin)/layout.tsx
+++ frontend/app/(admin)/layout.tsx
@@ -1,16 +1,22 @@
+"use client";
+
 import AdminNavBar from "@/components/ui/AdminNavBar";
 import AdminProfileButton from "@/components/ui/custom/adminProfileButton";
+import { usePathname } from "next/navigation";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isStatusPage = pathname.includes("/admin/order/status/");
+
   return (
-    <div className="admin-order-layout pb-32">
+    <div className={`admin-order-layout ${isStatusPage ? "" : "pb-32"}`}>
       <AdminProfileButton />
       {children}
-      <AdminNavBar />
+      {!isStatusPage && <AdminNavBar />}
     </div>
   );
 }
