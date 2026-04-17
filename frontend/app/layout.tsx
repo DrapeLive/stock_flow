0d799a040d4463cefa86dc8d1331b703bbf0d285
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2026-04-17 12:02:24 +0530

    fix: Fix logo and media folde error

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
@@ -13,6 +13,19 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Stock Flow",
   description: "Manage all your orders in one place",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
+  appleWebApp: {
+    capable: true,
+    title: "StockFlow",
+    statusBarStyle: "black-translucent",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
@@ -24,7 +37,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.variable} antialiased  py-4 px-5 `}>
         <AuthProvider>{children}</AuthProvider>
-        <Toaster position="top-right" richColors toastOptions={{ duration: 3000 }} />
+        <Toaster
+          position="top-right"
+          richColors
+          toastOptions={{ duration: 3000 }}
+        />
       </body>
     </html>
   );
--- frontend/app/manifest.ts
+++ frontend/app/manifest.ts
@@ -2,23 +2,26 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: "/",
     name: "Stock Flow",
     short_name: "StockFlow",
-    description: "Seamlessly control your item flow",
+    description: "Control your stock seamlessly",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
       },
     ],
   };
