915d7dee0ac395f75810010d1a5b520f9017c572
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2026-05-05 18:04:51 +0530

    fix: Allow fake default env for django (For docker build)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # SMTP Server
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST = config("EMAIL_HOST", default="")
 EMAIL_PORT = config("EMAIL_PORT", default=587)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@example.com")
 
 
 # Database
